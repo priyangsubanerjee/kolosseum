6bb397ebc92ea40a036afdb2575c50d2d04c12ba
Priyangsu Banerjee <priyangsu26@gmail.com>
2024-03-05 19:45:18 +0530

    Add metadata for Kolosseum hackathon

--- app/page.js
+++ app/page.js
@@ -11,6 +11,35 @@ export const viewport = {
   maximumScale: 1,
   userScalable: false,
 };
+
+export const metadata = {
+  title: "Ultimate hackathon, Kolosseum - Konnexions",
+  description:
+    "Gear up! Konnexions is back with the ultimate hackathon, Kolosseum. Brace yourself for an exhilarating journey of problem-solving, collaboration, and cutting-edge development. Whether you're a coding maestro or a budding genius, this is your chance to showcase your talent and make waves in the digital realm. Don't let this opportunity slip through your fingertips! Secure your spot now —register fast!",
+  icons: {
+    icon: "/icon.png",
+  },
+  openGraph: {
+    title: "Code. Collaborate. Conquer | Kolosseum",
+    description:
+      "Gear up! Konnexions is back with the ultimate hackathon, Kolosseum. Brace yourself for an exhilarating journey of problem-solving, collaboration, and cutting-edge development. Whether you're a coding maestro or a budding genius, this is your chance to showcase your talent and make waves in the digital realm. Don't let this opportunity slip through your fingertips! Secure your spot now —register fast!",
+    type: "website",
+    authors: ["Phyr Studios", "phyr"],
+    images: [
+      {
+        url: "https://kolesseum.konnexions.dev/og-image-800.png",
+        width: 800,
+        height: 600,
+      },
+      {
+        url: "https://kolesseum.konnexions.dev/og-image-800.png",
+        width: 1800,
+        height: 1600,
+      },
+    ],
+  },
+};
+
 export default function Home() {
   return (
     <main className="min-h-screen h-fit bg-neutral-100">
