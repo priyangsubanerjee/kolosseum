e9e7af7201b5303d98c23d9d96758fe78809d884
Priyangsu Banerjee <priyangsu26@gmail.com>
2024-03-06 00:15:04 +0530

    Update Form component styling

--- components/Form.js
+++ components/Form.js
@@ -249,7 +249,7 @@ function Form() {
       style={{
         pointerEvents: isLoading ? "none" : "all",
       }}
-      className="w-full md:w-full h-full md:h-fit bg-white md:bg-white lg:rounded-lg px-6 py-16 md:p-10 border-t border-dashed lg:border-none"
+      className="w-full md:w-full h-full relative md:h-fit bg-white md:bg-white lg:rounded-lg px-6 py-16 md:p-10 border-t border-dashed lg:border-none"
     >
       <div className="flex items-center">
         <svg
@@ -264,12 +264,12 @@ function Form() {
             <path d="M9.5 11.75a2 2 0 1 0 0-4a2 2 0 0 0 0 4m0 1a3 3 0 1 0 0-6a3 3 0 0 0 0 6" />
           </g>
         </svg>
-        <h1 className="text-xl md:text-2xl font-light ml-2">
+        <h1 className="text-2xl md:text-2xl font-light ml-2">
           <span className=" font-semibold">Team</span> registration
         </h1>
-        <Button className="text-neutral-900 bg-neutral-200 rounded-full hover:underline text-sm flex items-center ml-auto">
-          <div className="flex items-center md:px-3">
-            <span className="hidden md:block mr-1">Login</span>
+        <Button className="text-neutral-900 absolute top-0 md:static -translate-y-1/2 left-1/2 -translate-x-1/2 bg-neutral-200 rounded-full hover:underline text-sm flex items-center ml-auto">
+          <div className="flex items-center px-3">
+            <span className="mr-1">Login</span>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="23"
