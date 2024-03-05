7e4354d825f5045c05d6e5e5bb9a2c22d91c389b
Priyangsu Banerjee <priyangsu26@gmail.com>
2024-03-06 01:52:18 +0530

    Refactor code for email sending and button styling

--- app/register/api/send-email/route.js
+++ app/register/api/send-email/route.js
@@ -1,13 +1,1 @@
-export async function POST() {
-  await sendMail(
-    process.env.ZOHO_MAIL,
-    process.env.ZOHO_PASS,
-    ["22052490@kiit.ac.in"],
-    "Team Registered Successfully",
-    `Your team has been registered successfully for the event. Your team id is. Please use this id for further communication.`
-  );
-  return Response.json({
-    success: true,
-    message: "Team Registered Successfully",
-  });
-}
+export async function POST() {}
