c9ba7a760c6e8adfd551ebd766c46a744b30ba38
Priyangsu Banerjee <priyangsu26@gmail.com>
2024-03-07 03:42:55 +0530

    Add viewport and metadata for page.js

--- app/attendee/[tid]/page.js
+++ app/attendee/[tid]/page.js
@@ -8,6 +8,39 @@ import Link from "next/link";
 import React, { useEffect } from "react";
 import toast from "react-hot-toast";
 
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+};
+
+export const metadata = {
+  title: "Get Your Virtual Id | Kolosseum",
+  description:
+    "Gear up! Konnexions is back with the ultimate hackathon, Kolosseum. Brace yourself for an exhilarating journey of problem-solving, collaboration, and cutting-edge development. Whether you're a coding maestro or a budding genius, this is your chance to showcase your talent and make waves in the digital realm. Don't let this opportunity slip through your fingertips! Secure your spot now —register fast!",
+  icons: {},
+  openGraph: {
+    title: "Get Your Virtual Id | Kolosseum",
+    description:
+      "Gear up! Konnexions is back with the ultimate hackathon, Kolosseum. Brace yourself for an exhilarating journey of problem-solving, collaboration, and cutting-edge development. Whether you're a coding maestro or a budding genius, this is your chance to showcase your talent and make waves in the digital realm. Don't let this opportunity slip through your fingertips! Secure your spot now —register fast!",
+    type: "website",
+    authors: ["Konnexions", "KIIT"],
+    images: [
+      //   {
+      //     url: "https://kolesseum.vercel.app/og-image-800.png",
+      //     width: 800,
+      //     height: 600,
+      //   },
+      //   {
+      //     url: "https://kolesseum.vercel.app/og-image-800.png",
+      //     width: 1800,
+      //     height: 1600,
+      //   },
+    ],
+  },
+};
+
 function IdCards({ params }) {
   const [pageLoaded, setPageLoaded] = React.useState(false);
   const [spinnerOn, setSpinnerOn] = React.useState(true);
