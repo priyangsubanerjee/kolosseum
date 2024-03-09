053aee34be67b93ae6c6ffd5d09f360d84395870
Priyangsu Banerjee <priyangsu26@gmail.com>
2024-03-09 12:23:23 +0530

    Update event guide link in Eventdetails component

--- components/Eventdetails.js
+++ components/Eventdetails.js
@@ -121,24 +121,26 @@ function Eventdetails() {
               <span>Contact us</span>
             </button>
           </Link>
-          <button className="text-sm ml-auto text-neutral-700 hover:underline flex items-center space-x-2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="20"
-              height="20"
-              viewBox="0 0 512 512"
-            >
-              <path
-                fill="none"
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="32"
-                d="M384 224v184a40 40 0 0 1-40 40H104a40 40 0 0 1-40-40V168a40 40 0 0 1 40-40h167.48M336 64h112v112M224 288L440 72"
-              />
-            </svg>
-            <span>Event guide</span>
-          </button>
+          <Link href={"/eventguide.pdf"}>
+            <button className="text-sm ml-auto text-neutral-700 hover:underline flex items-center space-x-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                viewBox="0 0 512 512"
+              >
+                <path
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="32"
+                  d="M384 224v184a40 40 0 0 1-40 40H104a40 40 0 0 1-40-40V168a40 40 0 0 1 40-40h167.48M336 64h112v112M224 288L440 72"
+                />
+              </svg>
+              <span>Event guide</span>
+            </button>
+          </Link>
         </div>
       </div>
     </div>
