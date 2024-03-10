2ccafed2d2338b4f9c3fd96db8f183b0fdb7b702
Priyangsu Banerjee <priyangsu26@gmail.com>
2024-03-10 23:28:24 +0530

    Add server session authentication to API routes

--- app/register/api/route.js
+++ app/register/api/route.js
@@ -4,41 +4,50 @@ import { SaveToSheet } from "@/helper/saveToSheet";
 import { sendMail } from "@/helper/sendMail";
 import { RegisterTeam } from "@/prisma/team";
 import random from "random-string-generator";
+import { getServerSession } from "next-auth/next";
 
 export async function POST(request) {
-  let data = await request.json();
-  let teamname = data.teamName;
-  let arena = data.arena;
-  let gmail = data.gmail;
-  let totalParticipants = data.totalParticipants;
-  let members = [];
-  if (data.member1.name != "") {
-    members.push(data.member1);
+  let session = await getServerSession({ req: request });
+  if (session) {
+    let data = await request.json();
+    let teamname = data.teamName;
+    let arena = data.arena;
+    let gmail = data.gmail;
+    let totalParticipants = data.totalParticipants;
+    let members = [];
+    if (data.member1.name != "") {
+      members.push(data.member1);
+    }
+    if (data.member2.name != "") {
+      members.push(data.member2);
+    }
+    if (data.member3.name != "") {
+      members.push(data.member3);
+    }
+
+    let ticketProp = {
+      teamName: teamname,
+      arena: arena,
+      pid: random(7),
+      apiToken: random(32),
+      totalParticipants: totalParticipants,
+      members: members,
+      gmail: gmail,
+    };
+
+    let { success, message, pid } = await RegisterTeam(ticketProp);
+
+    console.error("Success: ", success, "Message: ", message);
+
+    return Response.json({
+      success: success,
+      message: message,
+      pid: pid,
+    });
+  } else {
+    return Response.json({
+      success: false,
+      message: "Unauthorized",
+    });
   }
-  if (data.member2.name != "") {
-    members.push(data.member2);
-  }
-  if (data.member3.name != "") {
-    members.push(data.member3);
-  }
-
-  let ticketProp = {
-    teamName: teamname,
-    arena: arena,
-    pid: random(7),
-    apiToken: random(32),
-    totalParticipants: totalParticipants,
-    members: members,
-    gmail: gmail,
-  };
-
-  let { success, message, pid } = await RegisterTeam(ticketProp);
-
-  console.error("Success: ", success, "Message: ", message);
-
-  return Response.json({
-    success: success,
-    message: message,
-    pid: pid,
-  });
 }
--- app/register/api/unique-name/route.js
+++ app/register/api/unique-name/route.js
@@ -1,11 +1,20 @@
 import { CheckUniqueName } from "@/prisma/team";
+import { getServerSession } from "next-auth/next";
 
 export async function POST(request) {
-  let { name } = await request.json();
-  let { success, message } = await CheckUniqueName(name);
+  let session = await getServerSession({ req: request });
+  if (session) {
+    let { name } = await request.json();
+    let { success, message } = await CheckUniqueName(name);
 
-  return Response.json({
-    success: success,
-    message: message,
-  });
+    return Response.json({
+      success: success,
+      message: message,
+    });
+  } else {
+    return Response.json({
+      success: false,
+      message: "Unauthorized",
+    });
+  }
 }
