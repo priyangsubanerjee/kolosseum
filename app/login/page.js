d8d09a2fdb7791bed9847088e2e045f2dec3cb41
Priyangsu Banerjee <priyangsu26@gmail.com>
2024-03-11 01:06:36 +0530

    Add LoginForm component to login page

--- app/login/page.js
+++ app/login/page.js
@@ -1,13 +1,17 @@
+/* eslint-disable @next/next/no-img-element */
+import { useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
 import React from "react";
+import LoginForm from "./components/LoginForm";
 
 function page() {
   if (process.env.NODE_ENV == "production") {
     return redirect("/");
   }
+
   return (
     <div className="min-h-screen bg-neutral-100 flex justify-center pt-24">
-      <div className="w-[500px] bg-white h-[600px] rounded-md"></div>
+      <LoginForm />
     </div>
   );
 }
