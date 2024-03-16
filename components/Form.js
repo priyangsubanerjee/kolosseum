fd4858e89cbc329fd6e6367c4650ad402f9e6a20
Priyangsu Banerjee <priyangsu26@gmail.com>
2024-03-16 17:53:53 +0530

    Update user authentication logic

--- components/Form.js
+++ components/Form.js
@@ -325,7 +325,7 @@ function Form() {
       >
         {session.status == "authenticated" ? (
           <>
-            <div className="relative">
+            <div className="">
               <div className="flex items-center">
                 <svg
                   className="h-10 w-10 lg:h-12 lg:w-12"
@@ -386,103 +386,125 @@ function Form() {
                   Goodies & giveaways
                 </span>
               </div>
-              <div className="mt-16">
-                {step < 5 && (
-                  <>
-                    <div className="flex items-center justify-center whitespace-nowrap scale-90 md:scale-100">
-                      {[1, 2, 3, 4, 5].map((i, index) => (
-                        <div
-                          key={i}
-                          className="flex items-center whitespace-nowrap"
-                        >
+              <div className="hidden">
+                <div className="mt-16">
+                  {step < 5 && (
+                    <>
+                      <div className="flex items-center justify-center whitespace-nowrap scale-90 md:scale-100">
+                        {[1, 2, 3, 4, 5].map((i, index) => (
                           <div
-                            style={{
-                              background: index <= step && "rgb(217 249 157)",
-                            }}
-                            className="flex items-center justify-center rounded-full bg-neutral-100 h-12 w-12"
+                            key={i}
+                            className="flex items-center whitespace-nowrap"
                           >
-                            <p>{i}</p>
+                            <div
+                              style={{
+                                background: index <= step && "rgb(217 249 157)",
+                              }}
+                              className="flex items-center justify-center rounded-full bg-neutral-100 h-12 w-12"
+                            >
+                              <p>{i}</p>
+                            </div>
+                            {i !== 5 && (
+                              <div className="w-[30px] lg:w-[50px] h-[1px] bg-neutral-200"></div>
+                            )}
                           </div>
-                          {i !== 5 && (
-                            <div className="w-[30px] lg:w-[50px] h-[1px] bg-neutral-200"></div>
-                          )}
+                        ))}
+                      </div>
+                      <div className="mt-10 text-sm">
+                        <span className="font-medium">{stepLabels[step]}</span>
+                        <span className="text-xs text-neutral-600 ml-3">
+                          <span className="text-red-400">*</span> fields are
+                          mandatory
+                        </span>
+                      </div>
+                    </>
+                  )}
+                </div>
+                <div className="mt-5">
+                  {step === 0 ? (
+                    <Teamname
+                      formProps={formProps}
+                      setFormProps={setFormProps}
+                    />
+                  ) : step === 1 ? (
+                    <Member1
+                      formProps={formProps}
+                      setFormProps={setFormProps}
+                    />
+                  ) : step === 2 ? (
+                    <Member2
+                      formProps={formProps}
+                      setFormProps={setFormProps}
+                    />
+                  ) : step === 3 ? (
+                    <Member3
+                      formProps={formProps}
+                      setFormProps={setFormProps}
+                    />
+                  ) : step === 4 ? (
+                    <Review formProps={formProps} setFormProps={setFormProps} />
+                  ) : step === 5 ? (
+                    <Success uid={uid} />
+                  ) : null}
+                </div>
+                <div className="mt-12">
+                  {step < 5 && (
+                    <div className="flex items-center justify-between mt-12">
+                      <Button
+                        disabled={step === 0}
+                        onClick={() => setStep((s) => s - 1)}
+                        className="bg-neutral-100 text-neutral-900 rounded-full disabled:opacity-45 disabled:cursor-not-allowed"
+                      >
+                        <div className="flex items-center space-x-2">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="20"
+                            height="20"
+                            viewBox="0 0 24 24"
+                          >
+                            <g fill="none" fill-rule="evenodd">
+                              <path d="M24 0v24H0V0zM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035c-.01-.004-.019-.001-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022m-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z" />
+                              <path
+                                fill="currentColor"
+                                d="M8.293 12.707a1 1 0 0 1 0-1.414l5.657-5.657a1 1 0 1 1 1.414 1.414L10.414 12l4.95 4.95a1 1 0 0 1-1.414 1.414z"
+                              />
+                            </g>
+                          </svg>
+                          <span>Back</span>
                         </div>
-                      ))}
-                    </div>
-                    <div className="mt-10 text-sm">
-                      <span className="font-medium">{stepLabels[step]}</span>
-                      <span className="text-xs text-neutral-600 ml-3">
-                        <span className="text-red-400">*</span> fields are
-                        mandatory
-                      </span>
-                    </div>
-                  </>
-                )}
-              </div>
-              <div className="mt-5">
-                {step === 0 ? (
-                  <Teamname formProps={formProps} setFormProps={setFormProps} />
-                ) : step === 1 ? (
-                  <Member1 formProps={formProps} setFormProps={setFormProps} />
-                ) : step === 2 ? (
-                  <Member2 formProps={formProps} setFormProps={setFormProps} />
-                ) : step === 3 ? (
-                  <Member3 formProps={formProps} setFormProps={setFormProps} />
-                ) : step === 4 ? (
-                  <Review formProps={formProps} setFormProps={setFormProps} />
-                ) : step === 5 ? (
-                  <Success uid={uid} />
-                ) : null}
-              </div>
-              <div className="mt-12">
-                {step < 5 && (
-                  <div className="flex items-center justify-between mt-12">
-                    <Button
-                      disabled={step === 0}
-                      onClick={() => setStep((s) => s - 1)}
-                      className="bg-neutral-100 text-neutral-900 rounded-full disabled:opacity-45 disabled:cursor-not-allowed"
-                    >
-                      <div className="flex items-center space-x-2">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="20"
-                          height="20"
-                          viewBox="0 0 24 24"
-                        >
-                          <g fill="none" fill-rule="evenodd">
-                            <path d="M24 0v24H0V0zM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035c-.01-.004-.019-.001-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022m-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z" />
+                      </Button>
+                      <Button
+                        onClick={() => handleProgress()}
+                        className="bg-black text-white rounded-full"
+                      >
+                        <div className="flex items-center space-x-4 px-2">
+                          <span>{step === 4 ? "Submit" : "Next"}</span>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="20"
+                            height="20"
+                            viewBox="0 0 24 24"
+                          >
                             <path
                               fill="currentColor"
-                              d="M8.293 12.707a1 1 0 0 1 0-1.414l5.657-5.657a1 1 0 1 1 1.414 1.414L10.414 12l4.95 4.95a1 1 0 0 1-1.414 1.414z"
+                              fill-rule="evenodd"
+                              d="M13.483 4.47a.75.75 0 0 1 1.06 0l6.988 7a.75.75 0 0 1 0 1.06l-6.988 7a.75.75 0 0 1-1.061-1.06l5.709-5.719L3 12.762a.75.75 0 0 1-.002-1.5l16.194-.01l-5.711-5.722a.75.75 0 0 1 0-1.06"
+                              clip-rule="evenodd"
                             />
-                          </g>
-                        </svg>
-                        <span>Back</span>
-                      </div>
-                    </Button>
-                    <Button
-                      onClick={() => handleProgress()}
-                      className="bg-black text-white rounded-full"
-                    >
-                      <div className="flex items-center space-x-4 px-2">
-                        <span>{step === 4 ? "Submit" : "Next"}</span>
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="20"
-                          height="20"
-                          viewBox="0 0 24 24"
-                        >
-                          <path
-                            fill="currentColor"
-                            fill-rule="evenodd"
-                            d="M13.483 4.47a.75.75 0 0 1 1.06 0l6.988 7a.75.75 0 0 1 0 1.06l-6.988 7a.75.75 0 0 1-1.061-1.06l5.709-5.719L3 12.762a.75.75 0 0 1-.002-1.5l16.194-.01l-5.711-5.722a.75.75 0 0 1 0-1.06"
-                            clip-rule="evenodd"
-                          />
-                        </svg>
-                      </div>
-                    </Button>
-                  </div>
-                )}
+                          </svg>
+                        </div>
+                      </Button>
+                    </div>
+                  )}
+                </div>
+              </div>
+
+              <div className="flex items-center justify-center">
+                <img
+                  src="https://www.steelmintevents.com/wp-content/uploads/2023/03/Untitled-1.png"
+                  className="w-56 md:w-64 mt-12 object-contain"
+                  alt=""
+                />
               </div>
               <div className="mt-32 md:mt-10">
                 <div className="flex items-center justify-center">
@@ -500,14 +522,6 @@ function Form() {
                 </div>
               </div>
               <div className="h-44 md:hidden"></div>
-
-              <div className="absolute inset-0 bg-white/80 pointer-events-none select-none z-20 flex items-center justify-center">
-                <img
-                  src="https://www.steelmintevents.com/wp-content/uploads/2023/03/Untitled-1.png"
-                  className="h-44"
-                  alt=""
-                />
-              </div>
             </div>
           </>
         ) : (
